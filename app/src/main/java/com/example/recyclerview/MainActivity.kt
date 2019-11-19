7c45d56676b85e122b420f4f4354d15e700da055
Rameez Shah <rshah@servicechannel.com>
2019-11-19 15:52:00 -0800

    Option to convert to grid

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.recyclerview
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlin.random.Random
@@ -16,8 +17,11 @@ class MainActivity : AppCompatActivity() {
         val adapter = RandomNumberAdapter()
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // to display in grid format
+//        recyclerView.layoutManager = GridLayoutManager(this, 3)
 
         findViewById<Button>(R.id.add_btn).setOnClickListener {
             val randomNumber = RandomNumber(Random.nextInt(0, 100))
