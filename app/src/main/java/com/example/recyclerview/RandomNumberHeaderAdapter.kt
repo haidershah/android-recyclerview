c962f93b91bacf2e13be034605bbdaf62d415291
Rameez Shah <rshah@servicechannel.com>
2019-11-20 10:48:35 -0800

    RecyclerView list with header

--- app/src/main/java/com/example/recyclerview/RandomNumberHeaderAdapter.kt
+++ app/src/main/java/com/example/recyclerview/RandomNumberHeaderAdapter.kt
@@ -5,31 +5,73 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.lang.ClassCastException
 
-class RandomNumberHeaderAdapter : RecyclerView.Adapter<RandomNumberHeaderAdapter.ViewHolder>() {
-    private val data = mutableListOf<RandomNumber>()
+private const val ITEM_VIEW_TYPE_HEADER = 0
+private const val ITEM_VIEW_TYPE_ITEM = 1
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+class RandomNumberHeaderAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val data = mutableListOf<DataItem>()
+
+    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val numberView: TextView = view.findViewById(R.id.number)
+    }
+
+    class HeaderViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val headerView: TextView = view.findViewById(R.id.header)
     }
 
     override fun getItemCount(): Int {
         return data.size
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.list_item_random_number_header, parent, false)
-        return ViewHolder(view)
+
+        return when (viewType) {
+            ITEM_VIEW_TYPE_HEADER -> {
+                val view =
+                    layoutInflater.inflate(R.layout.list_item_random_number_header, parent, false)
+                HeaderViewHolder(view)
+            }
+            ITEM_VIEW_TYPE_ITEM -> {
+                val view =
+                    layoutInflater.inflate(R.layout.list_item_random_number_item, parent, false)
+                ItemViewHolder(view)
+            }
+            else -> {
+                throw ClassCastException("Unknown viewType $viewType")
+            }
+        }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = data[position]
-        holder.numberView.text = item.number.toString()
+    override fun getItemViewType(position: Int): Int {
+        return when (data[position]) {
+            is DataItem.Header -> ITEM_VIEW_TYPE_HEADER
+            is DataItem.RandomNumberItem -> ITEM_VIEW_TYPE_ITEM
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is ItemViewHolder -> {
+                val item = data[position] as DataItem.RandomNumberItem
+                holder.numberView.text = item.randomNumber.number.toString()
+            }
+        }
     }
 
     fun addData(randomNumbers: List<RandomNumber>) {
-        data.addAll(randomNumbers)
+        data.add(DataItem.Header)
+
+        data.addAll(randomNumbers.map {
+            DataItem.RandomNumberItem(it)
+        })
         notifyDataSetChanged()
     }
+}
+
+sealed class DataItem {
+    data class RandomNumberItem(val randomNumber: RandomNumber) : DataItem()
+    object Header : DataItem()
 }
